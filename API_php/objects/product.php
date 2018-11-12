c4464c1859af8a9823cea557b369c8d69d4f9223
DanielGeek <daniel30081990@gmail.com>
2018-11-12 01:40:13 -0300

    Agregado paging API

--- API_php/objects/product.php
+++ API_php/objects/product.php
@@ -175,9 +175,9 @@ class Product{
         
     }
     // search products
-function search($keywords){
+    function search($keywords){
  
-    // select all query
+        // select all query
         $query = "SELECT
                     c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
                 FROM
@@ -206,5 +206,42 @@ function search($keywords){
         $stmt->execute();
     
         return $stmt;
+    }
+    // read products with pagination
+    public function readPaging($from_record_num, $records_per_page){
+    
+        // select query
+        $query = "SELECT
+                    c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+                FROM
+                    " . $this->table_name . " p
+                    LEFT JOIN
+                        categories c
+                            ON p.category_id = c.id
+                ORDER BY p.created DESC
+                LIMIT ?, ?";
+    
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );
+    
+        // bind variable values
+        $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
+    
+        // execute query
+        $stmt->execute();
+    
+        // return values from database
+        return $stmt;
+    }
+    // used for paging products
+    public function count(){
+        $query = "SELECT COUNT(*) as total_rows FROM " . $this->table_name . "";
+    
+        $stmt = $this->conn->prepare( $query );
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+        return $row['total_rows'];
     }
 }
